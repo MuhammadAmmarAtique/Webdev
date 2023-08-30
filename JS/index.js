fa168db33fca96955e12f76b50a6f8a3f6767a8f
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-08-31 00:02:49 +0500

    Learne dvariables and operators

---
+++ JS/index.js
@@ -0,0 +1,40 @@
+// Learning JS
+
+//1.VARIABLES:-
+
+// let a="East or West let for making variable is best ";
+// console.log(a);
+
+// 2.OPERATORS:-
+
+// let x=34.5;
+// console.log(x);
+// x=-x;
+// console.log(x);
+
+// let g=59;
+// console.log(g);
+// g=59+11;
+// console.log(g);
+
+// let f=9;
+// let g=8;
+// console.log("The answer of f+g is:" + (f+g));
+// console.log("The answer of f-g is:" + (f-g));
+// console.log("The answer of f*g is:" + (f*g));
+// console.log("The answer of f/g is:" + (f/g));
+
+// let a=2;
+// let b=3;
+// console.log(a**3);
+
+// let a=1;
+// // console.log(++a);
+// console.log(a++);
+// console.log(a);
+
+
+// let b=3;
+// // console.log(--b);
+// console.log(b--);
+// console.log(b);
